3fd7ded679a520c84073b948ac327620f437f1f0
yashita99 <45969204+yashita99@users.noreply.github.com>
2021-05-31 14:27:57 +0530

    Update Ff.vue

--- src/components/Ff.vue
+++ src/components/Ff.vue
@@ -39,5 +39,7 @@ export default {
 
 
 <style>
-@import "../node_modules/@syncfusion/ej2-vue-grids/styles/material.css";
+@import '../../node_modules/@syncfusion/ej2-base/styles/material.css';  
+@import '../../node_modules/@syncfusion/ej2-buttons/styles/material.css';
+@import "../../node_modules/@syncfusion/ej2-vue-grids/styles/material.css";
 </style>
